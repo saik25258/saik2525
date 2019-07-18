9e29e4cdf78cbd7cb805abc7022ad7fe2b55876d
saik25258 <53034321+saik25258@users.noreply.github.com>
2019-07-18 15:15:20 +0530

    Create fourth.py

---
+++ fourth.py
@@ -0,0 +1,9 @@
+a1=input()
+a2=input()
+a3=input()
+if (a1 >= a2) and (a1 >= a3):
+   print("largest = a1")
+elif (a2 >= a1) and (a2 >= a3):
+   print("largest = a2")
+else:
+   print("largest = a3")
