7f2c1e3c9141c599d490e4ed166dad4a7cdb02d7
saik25258 <53034321+saik25258@users.noreply.github.com>
2019-07-18 15:17:40 +0530

    Update fourth.py

--- fourth.py
+++ fourth.py
@@ -2,8 +2,8 @@ a1=input()
 a2=input()
 a3=input()
 if (a1 >= a2) and (a1 >= a3):
-   print("largest = a1")
+   print("Largest = a1")
 elif (a2 >= a1) and (a2 >= a3):
-   print("largest = a2")
+   print("Largest = a2")
 else:
-   print("largest = a3")
+   print("Largest = a3")
